bfae6e0f822798e626724e00854c12136b632aeb
Derek Santos <derek@dyrenex.com>
2020-11-23 16:15:30 -0500

    Added technologies and tools lists to home() so I dont have to keep adding it myself.

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,53 @@ def create_app():
     @app.route('/', methods=["GET"])
     def home():
         """The index / home route of the website"""
-        return render_template('home.html')
+
+        # Technology, Stars out of 5
+        technologies = [
+            ('Python', 5),
+            ('Java', 5),
+            ('C++', 5),
+            ('CSS', 5),
+            ('Javascript', 5),
+            ('Git', 5),
+            ('Docker', 5),
+            ('LXC / LXD', 5),
+            ('Ansible', 4),
+            ('Open Computer Vision', 5),
+            ('NGINX', 4),
+            ('Flask', 5),
+            ('Github', 5),
+            ('Gitlab', 5),
+            ('Elasticsearch', 3),
+            ('Kibana', 3),
+            ('Logstash', 3),
+            ('REST APIs', 5),
+            ('JSON', 5),
+            ('YAML', 5),
+            ('Kubernetes', 4)
+        ]
+        tools = [
+            ('Proxmox', 5),
+            ('Debian', 5),
+            ('Ubuntu', 5),
+            ('Visual Studio 2019', 4),
+            ('Manjaro', 5),
+            ('Windows', 5),
+            ('VSCode', 5),
+            ('VMWare', 5),
+            ('ESXI', 5),
+        ]
+
+        technologies.sort(key=lambda x: x[1], reverse = True)
+        tools.sort(key=lambda x: x[1], reverse = True)
+        
+        technologies_left=technologies[:len(technologies) // 2]
+        technologies_right=technologies[len(technologies) // 2:]
+
+        return render_template('home.html',
+                               technologies_left=technologies_left,
+                               technologies_right=technologies_right,
+                               tools=tools)
 
     @app.route('/github', methods=["GET"])
     def github():
