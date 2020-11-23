8056b0dd8b0a1af6bda638649245440137b5d802
Derek Santos <derek@dyrenex.com>
2020-11-23 14:46:15 -0500

    Added parallax to the page. Added smooth scrolling for links. Added stock image.

---
+++ website/static/js/base.js
@@ -0,0 +1,16 @@
+// window.scroll({
+//     top: 2500,
+//     left: 0,
+//     behavior: 'smooth'
+// });
+
+// window.scrollBy({
+//     top: 100,
+//     left: 0,
+//     behavior: 'smooth'
+// });
+
+window.onload = function () {
+    $("#loading").hide();
+    $("#content").show();
+}
