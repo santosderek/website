e6caff2f421928358253311d591d702fe24b61d5
Derek Santos <derek@dyrenex.com>
2020-11-23 21:10:10 -0500

    Latest. Formatting.

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -1,16 +1,6 @@
-// window.scroll({
-//     top: 2500,
-//     left: 0,
-//     behavior: 'smooth'
-// });
-
-// window.scrollBy({
-//     top: 100,
-//     left: 0,
-//     behavior: 'smooth'
-// });
-
 window.onload = function () {
     $("#loading").hide();
     $("#content").show();
+
 }
+
