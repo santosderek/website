c0617daabab9af39d0b61aa742f43df85a8d81c4
Derek Santos <derek@dyrenex.com>
2020-11-23 21:55:47 -0500

    Added ability for #pageLocation to hide itself if above #experience.

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -1,6 +1,18 @@
 window.onload = function () {
     $("#loading").hide();
     $("#content").show();
+    $('body').scrollspy({ target: '#pageLocation' })
 
+    // Hide fixed nav if above Experience jumbotron
+    $("#pageLocation").hide(); //hide your div initially
+    var topOfOthDiv = $("#experience").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#pageLocation").show(); //reached the desired point -- show div
+        }
+        else{
+            $("#pageLocation").hide(); //else above the desired point -- hide div
+        }
+    });
 }
 
