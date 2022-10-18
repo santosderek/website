25ec6bc3415de2d17d38c66316222f38a4d51d83
Derek Santos <derek@dyrenex.com>
2022-10-17 21:58:49 -0400

    Added robots file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 if [[ "$FLASK_ENV" == "gunicorn" ]]; then
     gunicorn -w 4 -b 0.0.0.0:8000 --worker-tmp-dir /dev/shm "website:create_app()"
 elif [[ "$FLASK_ENV" == "flask" ]]; then
